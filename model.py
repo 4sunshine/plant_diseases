540154ec7af0c337419533b6fd76bec5ff47f641
4sunshine <alexandershain.5@gmail.com>
2020-06-05 02:34:31 +0300

    Stupid quant

--- model.py
+++ model.py
@@ -4,15 +4,32 @@ import torch
 
 
 def all_sliding_windows(a, stride=[4, 4], mask_size=[17, 17]):
+    """RETURNS ARRAY OF SHAPE (B, C, Nx, Ny, MaskX, MaskY)"""
     shape = a.shape[:-2] + ((a.shape[-2] - mask_size[-2]) // stride[-2] + 1, ) + \
             ((a.shape[-1] - mask_size[-1]) // stride[-1] + 1,) + tuple(mask_size)
-    strides = a.strides[:-2] + (a.strides[-2] * stride[-2],) + (a.strides[-1] * stride[-1],) + a.strides[-2:]
+    strides = a.strides[:-2] + (a.strides[-2] * stride[-2],) + (a.strides[-1] * stride[-1],) +\
+              a.strides[-2:]
     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
 
-class Leafnet(nn.Module):
-    '''REQUIRES TENSOR INPUT'''
+def statistics(a, axis=(-2, -1)):
+    means = a.mean(axis=axis)
+    std = a.std(axis=axis)
+    maximums = a.max(axis=axis)
+    minimums = a.min(axis=axis)
+    return means, std, maximums, minimums
 
+
+def quantize_stat(a, mean, std, mx, mn):
+    bins = np.array([0, mean-std, mean, mean+std, mx])
+
+
+
+
+
+
+class Leafnet(nn.Module):
+    """REQUIRES TENSOR INPUT"""
     def __init__(self):
         super().__init__()
         self.layer1 = GLCM()
@@ -41,11 +58,8 @@ class GLCM(nn.Module):
         images = images.detach().numpy()
         b, c, h, w = images.shape
         assert (h == self.images_size[0] + 1) and (w == self.images_size[0] + 1)
-        z = all_sliding_windows(images)
-        print(np.max(z[:,:,41,30,:,:]))
-        print(np.max(images[:,:, 8 + 4*41 - 8:8 + 4*41 + 9, 8 + 4*30 - 8:8 + 4*30 + 9]))
-        print(np.min(z[:,:,41,30,:,:]-images[:,:, 8 + 4*41 - 8:8 + 4*41 + 9, 8 + 4*30 - 8:8 + 4*30 + 9]))
-        #print(np.shape(all_sliding_windows(images)))
-
-        return b
+        sw = all_sliding_windows(images)
+        m, s, mx, mn = statistics(sw)
+        sw_q = quantize_stat(sw, statistics(sw))
+        return np.shape(m)
 
