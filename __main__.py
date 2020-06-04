59c35d91c2dba5baadfcd5589e745dcf6d870382
4sunshine <alexandershain.5@gmail.com>
2020-06-04 21:32:21 +0300

    Loaders ready

--- __main__.py
+++ __main__.py
@@ -1,11 +1,20 @@
 import torch
 from torchvision import transforms, utils
 from utils import PlantDiseaseDataset, splitted_loaders
+import numpy as np
+import random
 
 
 if __name__ == '__main__':
+    '''TESTING SETTINGS'''
+    np.random.seed(42)
+    random.seed(1000)
+    torch.backends.cudnn.determenistic = True
+    torch.backends.cudnn.benchmark = False
+    torch.manual_seed(1001)
+
     '''DATASET PARAMS'''
-    dataset_info = '/home/sunshine/irishka/0.3v/json/ds.json'
+    dataset_info = '/home/sunshine/irishka/0.3v/json/src_ds.json'
     root_dir = '/home/sunshine/irishka/0.3v/ds/src'
     batch_size = 16
     train_size = .75
@@ -15,4 +24,8 @@ if __name__ == '__main__':
     '''LOADERS OF DATA'''
     train_loader, test_loader = splitted_loaders(dataset, batch_size=batch_size, train_size=train_size)
 
+    for i, a in enumerate(train_loader):
+        print(a['labels'])
+        if i == 3:
+            break
 
--- utils.py
+++ utils.py
@@ -11,7 +11,6 @@ def splitted_loaders(dataset, train_size, batch_size):
     dataset_size = len(dataset)
     indices = list(range(dataset_size))
     split = int(np.floor(train_size * dataset_size))
-    #np.random.seed(random_seed)
     np.random.shuffle(indices)
     train_indices, val_indices = indices[split:], indices[:split]
 
@@ -20,16 +19,16 @@ def splitted_loaders(dataset, train_size, batch_size):
     valid_sampler = SubsetRandomSampler(val_indices)
 
     train_loader = DataLoader(dataset, batch_size=batch_size,
-                              sampler=train_sampler)
+                              sampler=train_sampler, shuffle=False)
     test_loader = DataLoader(dataset, batch_size=batch_size,
-                             sampler=valid_sampler)
+                             sampler=valid_sampler, shuffle=False)
     return train_loader, test_loader
 
 
-def shuffled_arrays(a, b):
-    assert len(a) == len(b)
-    p = np.random.permutation(len(a))
-    return a[p], b[p]
+# def shuffled_arrays(a, b):
+#     assert len(a) == len(b)
+#     p = np.random.permutation(len(a))
+#     return a[p.astype(int)], b[p.astype(int)]
 
 
 class PlantDiseaseDataset(Dataset):
@@ -44,7 +43,7 @@ class PlantDiseaseDataset(Dataset):
             data = json.load(f)
         paths = [os.path.join(data['group_names'][g], f) for f, g in
                  zip(data['image_names'], data['group_id'])]
-        return shuffled_arrays(paths, data['group_id']), {k: v for k, v in enumerate(data['group_names'])}
+        return paths, data['group_id'], {k: v for k, v in enumerate(data['group_names'])}
 
     def label_map(self, label):
         return self.map[label]
@@ -56,11 +55,13 @@ class PlantDiseaseDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         img_name = os.path.join(self.root, self.paths[idx])
-        image = Image.open(img_name)
+        image = Image.open(img_name).convert('RGB')
+        # H, W, [R,G,B]
+        # RETURN ONLY RED CHANNEL
+        image = np.array(image)[:, :, 0]
 
         if self.transform:
             image = self.transform(image)
 
-        print(np.shape(image))
-        return image, self.labels[idx], img_name
+        return {'images': image, 'labels': self.labels[idx], 'path': img_name}
 
