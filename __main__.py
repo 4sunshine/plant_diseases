b22efa55323a59b063d7e93dfe5c8db026f603c7
4sunshine <alexandershain.5@gmail.com>
2020-06-05 19:58:49 +0300

    Statistics nonzero

--- __main__.py
+++ __main__.py
@@ -1,24 +1,25 @@
 import torch
 from torchvision import transforms, utils
-from utils import PlantDiseaseDataset, splitted_loaders
+from dataset import PlantDiseaseDataset, splitted_loaders
 import numpy as np
 import random
 from model import Leafnet
-
+from utils import nonzero_stats, aggregate_stats
+from tqdm import tqdm
 
 if __name__ == '__main__':
     '''TESTING SETTINGS'''
     np.random.seed(42)
-    random.seed(1000)
+    random.seed(1001)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    torch.manual_seed(1001)
+    torch.manual_seed(1002)
 
     '''DATASET PARAMS'''
     dataset_info = '/home/sunshine/irishka/0.3v/json/src_ds.json'
     root_dir = '/home/sunshine/irishka/0.3v/ds/src'
-    batch_size = 16
-    train_size = .75
+    batch_size = 64
+    train_size = 1 #.75
 
     dataset = PlantDiseaseDataset(dataset_info, root_dir)
 
@@ -28,10 +29,7 @@ if __name__ == '__main__':
     '''MODEL'''
     model = Leafnet()
 
-    for i, data in enumerate(train_loader):
-        print(torch.max(data['images'][0]))
-        print(data['images'].shape)
-        print(model(data['images']))
-        if i == 0:
-            break
+    #  CALCULATED PARAMETERS. MEAN NONZERO RED = 85.384; MEAN NONZERO RED STD = 53.798
+    #  m, s = aggregate_stats(train_loader)
+    #
 
--- utils.py
+++ utils.py
@@ -1,74 +1,31 @@
-import json
 import torch
-import os
-from torch.utils.data import Dataset, DataLoader
-from PIL import Image
-import numpy as np
-from torch.utils.data.sampler import SubsetRandomSampler
+from tqdm import tqdm
 
 
-def splitted_loaders(dataset, train_size, batch_size):
-    dataset_size = len(dataset)
-    indices = list(range(dataset_size))
-    split = int(np.floor(train_size * dataset_size))
-    np.random.shuffle(indices)
-    train_indices, val_indices = indices[split:], indices[:split]
-
-    # Creating PT data samplers and loaders:
-    train_sampler = SubsetRandomSampler(train_indices)
-    valid_sampler = SubsetRandomSampler(val_indices)
-
-    train_loader = DataLoader(dataset, batch_size=batch_size,
-                              sampler=train_sampler, shuffle=False)
-    test_loader = DataLoader(dataset, batch_size=batch_size,
-                             sampler=valid_sampler, shuffle=False)
-    return train_loader, test_loader
+def nonzero_stats(images):
+    images = torch.flatten(images)
+    n_nonzeros = images.numel() - (images == 0).sum()
+    sum = images.sum()
+    std = torch.std(images.type(torch.DoubleTensor))
+    return n_nonzeros, sum, std
 
 
-# def shuffled_arrays(a, b):
-#     assert len(a) == len(b)
-#     p = np.random.permutation(len(a))
-#     return a[p.astype(int)], b[p.astype(int)]
+def aggregate_stats(data_loader):
+    """COMMON STATS"""
+    s = torch.tensor(0)
+    count = torch.tensor(0)
+    stds = []
 
-
-class PlantDiseaseDataset(Dataset):
-
-    def __init__(self, json_file, root_dir, transform=None):
-        self.root = root_dir
-        self.transform = transform
-        self.paths, self.labels, self.map = self._load_info(json_file)
-
-    def _load_info(self, json_file):
-        with open(json_file) as f:
-            data = json.load(f)
-        paths = [os.path.join(data['group_names'][g], f) for f, g in
-                 zip(data['image_names'], data['group_id'])]
-        return paths, data['group_id'], {k: v for k, v in enumerate(data['group_names'])}
-
-    def label_map(self, label):
-        return self.map[label]
-
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        img_name = os.path.join(self.root, self.paths[idx])
-        image = Image.open(img_name).convert('RGB')
-        # H, W, [R,G,B]
-        # RETURN ONLY RED CHANNEL
-        image = np.array(image)[:, :, 0]
-        image = np.expand_dims(image, axis=-1)
-        # CONVERT TO PYTORCH CONVENTION:
-        # N, C, H, W
-        image = np.transpose(image, (2, 0, 1))
-        # PAD IMAGES
-        image = np.pad(image, [(0, 0), (0, 1), (0, 1)], mode='constant')
-        image = torch.from_numpy(image)
-
-        if self.transform:
-            image = self.transform(image)
-
-        return {'images': image, 'labels': self.labels[idx], 'path': img_name}
+    with tqdm(total=len(data_loader)) as pbar:
+        for i, data in enumerate(data_loader):
+            cnt, sm, std = nonzero_stats(data['images'])
+            count += cnt
+            s += sm
+            stds += [std]
+            pbar.update(1)
+    mean = s / (count + 1e-7)
+    #std = torch.cat(stds, dim=0)
+    std = torch.mean(torch.tensor(stds))
+    print(std)
+    return mean, std
 
