b3605cd8a230b205aeb25829479d7e8a3fe4d43b
4sunshine <alexandershain.5@gmail.com>
2020-10-04 10:37:29 +0300

    Classifiers stored in dict

--- evaluate.py
+++ evaluate.py
@@ -15,27 +15,25 @@ from dataset import features_labels
 
 
 def build_classifiers():
-    classifiers_names = ('Decision Tree', 'Support Vector Machine', 'Random Forest',
-                         'K Nearest Neighbors', '1 Layer MLP')
-
-    classifiers = list()
-    classifiers.append(DecisionTreeClassifier(random_state=0, splitter='best', criterion='entropy', max_depth=3))
-    classifiers.append(SVC(gamma='auto', kernel='linear', C=5, shrinking=True))
-    classifiers.append(
-        RandomForestClassifier(max_depth=10, random_state=0, criterion='entropy', n_jobs=8, n_estimators=250))
-    classifiers.append(KNeighborsClassifier(n_neighbors=11, n_jobs=8, metric='euclidean'))
-    classifiers.append(MLPClassifier(activation='relu', alpha=0.0001, batch_size='auto',
-                                     beta_1=0.9, beta_2=0.999, early_stopping=False, epsilon=1e-08,
-                                     hidden_layer_sizes=(200,), learning_rate='constant',
-                                     learning_rate_init=0.001, max_fun=15000, max_iter=10000,
-                                     momentum=0.9, n_iter_no_change=10, nesterovs_momentum=True,
-                                     power_t=0.5, random_state=1, shuffle=True, solver='adam',
-                                     tol=0.0001, validation_fraction=0.1, verbose=False,
-                                     warm_start=False))
-    return tuple(classifiers), classifiers_names
+    classifiers = dict()
+    classifiers['decision_tree'] = DecisionTreeClassifier(random_state=0,
+                                                          splitter='best', criterion='entropy', max_depth=3)
+    classifiers['svm'] = SVC(gamma='auto', kernel='linear', C=5, shrinking=True)
+    classifiers['random_forest'] = RandomForestClassifier(max_depth=10, random_state=0,
+                                                          criterion='entropy', n_jobs=8, n_estimators=250)
+    classifiers['knn'] = KNeighborsClassifier(n_neighbors=11, n_jobs=8, metric='euclidean')
+    classifiers['1_layer_mlp'] = MLPClassifier(activation='relu', alpha=0.0001, batch_size='auto',
+                                               beta_1=0.9, beta_2=0.999, early_stopping=False, epsilon=1e-08,
+                                               hidden_layer_sizes=(200,), learning_rate='constant',
+                                               learning_rate_init=0.001, max_fun=15000, max_iter=10000,
+                                               momentum=0.9, n_iter_no_change=10, nesterovs_momentum=True,
+                                               power_t=0.5, random_state=1, shuffle=True, solver='adam',
+                                               tol=0.0001, validation_fraction=0.1, verbose=False,
+                                               warm_start=False)
+    return classifiers
 
 
-def classification_results(classifiers, classifiers_names, train_file, test_file):
+def classification_results(classifiers, train_file, test_file):
     # INDICES [FEATURE [: 68], STAT: [:4], HIST [4: 8], GLCM[8: 68]]
     ind = [list(range(68)), list(range(4)), list(range(8)), list(range(8, 68))]
     properties = ('FEATURE', 'STAT', 'STAT+HIST', 'GLCM')
@@ -47,7 +45,7 @@ def classification_results(classifiers, classifiers_names, train_file, test_file
     best_f = 0.
     best_classifier = None
 
-    for clf, name in tqdm(zip(classifiers, classifiers_names)):
+    for name, clf in tqdm(classifiers.items()):
         for i, prop in enumerate(properties):
             for standard in [True, False]:
                 clf, metrics = train_and_test(ind=ind[i], clf=clf, train_data=train_data,
@@ -73,9 +71,9 @@ if __name__ == '__main__':
     train_file = os.path.join(root_dir, 'train_data.pth')
     test_file = os.path.join(root_dir, 'test_data.pth')
 
-    classifiers, classifiers_names = build_classifiers()
+    classifiers = build_classifiers()
 
-    data, best_classifier = classification_results(classifiers, classifiers_names, train_file, test_file)
+    data, best_classifier = classification_results(classifiers, train_file, test_file)
 
     classifier, mean, std, ind = best_classifier
 
